fe1c5524eb2e38bb007255c08cf01e833c19e50b
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-03-31 17:11:05 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Catherine Vu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Catherine Vu
+// Created on: March 27 2025
 // This file contains the JS functions for index.html
+
+
+/**
+* Enter base A and B and height
+*/
+
+function calculateTrapezoidArea() {
+            let a = parseFloat(document.getElementById('baseA').value);
+            let b = parseFloat(document.getElementById('baseB').value);
+            let h = parseFloat(document.getElementById('height').value);
+            if (!isNaN(a) && !isNaN(b) && !isNaN(h)) {
+                let area = ((a + b) / 2) * h;
+                document.getElementById('area').innerText = `Area is: ${area} mm²`;
+            } else {
+                document.getElementById('area').innerText = 'Please enter valid numbers';
+            }
+        }
